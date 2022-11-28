4b5f0b5d7809d4ac856db9ea7d56732e96ee630d
Joshua Ibrom <joshuaibrom@gmail.com>
2022-11-27 20:32:00 -0600

    Finished the mobile menu

--- src/components/Navbar/MobileNavigation.svelte
+++ src/components/Navbar/MobileNavigation.svelte
@@ -6,11 +6,46 @@
   const toggleMenu = () => showMenu = !showMenu;
 </script>
 
-<button on:click={toggleMenu} aria-controls="mobile-nav-links">Menu</button>
+<button class="menu-controller" on:click={toggleMenu} aria-controls="mobile-nav-links" aria-expanded={showMenu}>
+  <svg class="hamburger" viewBox="-10 -10 120 120" width="32">
+    <!--
+    <rect class="line top" width="80" height="10" x="10" y="25" rx="5" />
+    <rect class="line middle" width="80" height="10" x="10" y="45" rx="5" />
+    <rect class="line bottom" width="80" height="10" x="10" y="65" rx="5" />
+    -->
+    <path
+      class="line"
+      stroke-width="10"
+      stroke-linecap="round"
+      stroke-joincap="round"
+      d="m 20 40 h 60 a 1 1 0 0 1 0 20 h -60 a 1 1 0 0 1 0 -40 h 30 v 70" />
+  </svg>
+</button>
 {#if showMenu}
-  <nav class="fixed left-0 right-0 h-full flex flex-col gap-2 nav-item transition-all" id="mobile-nav-links" aria-expanded="false">
+  <nav class="fixed left-0 right-0 h-full flex flex-col gap-2 nav-item transition-all" id="mobile-nav-links">
     {#each links as link}
       <a href={link.href}>{link.title}</a>
     {/each}
   </nav>
 {/if}
+
+<style>
+  .menu-controller {
+    @apply border-2 border-zinc-200 rounded-md overflow-hidden transition-all;
+  }
+  .hamburger {
+    @apply fill-none stroke-zinc-100 transition-all duration-700;
+  }
+  .hamburger .line {
+    @apply transition-all duration-1000;
+    stroke-dasharray: 60 31 60 1000;
+  }
+  .menu-controller[aria-expanded="true"] .hamburger {
+    translate: 2px -2px;
+    rotate: 0.125turn;
+  }
+  .menu-controller[aria-expanded="true"] .hamburger .line {
+    stroke-dasharray: 60 105 60 300;
+    stroke-dashoffset: -90;
+  }
+</style>
