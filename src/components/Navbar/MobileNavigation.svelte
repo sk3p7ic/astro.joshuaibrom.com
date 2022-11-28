5fc5711f6234af4d11c79569ad5635cd9dcab3f8
Joshua Ibrom <joshuaibrom@gmail.com>
2022-11-27 19:47:02 -0600

    Mobile menu now full screen height

--- src/components/Navbar/MobileNavigation.svelte
+++ src/components/Navbar/MobileNavigation.svelte
@@ -8,7 +8,7 @@
 
 <button on:click={toggleMenu} aria-controls="mobile-nav-links">Menu</button>
 {#if showMenu}
-  <nav class="fixed left-0 right-0 flex flex-col gap-2 nav-item" id="mobile-nav-links" aria-expanded="false">
+  <nav class="fixed left-0 right-0 h-full flex flex-col gap-2 nav-item transition-all" id="mobile-nav-links" aria-expanded="false">
     {#each links as link}
       <a href={link.href}>{link.title}</a>
     {/each}
