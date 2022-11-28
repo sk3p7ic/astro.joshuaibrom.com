6091cf211796efdde0054b1401235fb99a925377
Joshua Ibrom <joshuaibrom@gmail.com>
2022-11-27 19:44:08 -0600

    Mobile menu now toggle-able

---
+++ src/components/Navbar/MobileNavigation.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let links;
+
+  let showMenu = false;
+
+  const toggleMenu = () => showMenu = !showMenu;
+</script>
+
+<button on:click={toggleMenu} aria-controls="mobile-nav-links">Menu</button>
+{#if showMenu}
+  <nav class="fixed left-0 right-0 flex flex-col gap-2 nav-item" id="mobile-nav-links" aria-expanded="false">
+    {#each links as link}
+      <a href={link.href}>{link.title}</a>
+    {/each}
+  </nav>
+{/if}
