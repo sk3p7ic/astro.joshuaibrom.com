66ae00c2e4e6537f5b14650ee3ca5f275d52028b
Joshua Ibrom <joshuaibrom@gmail.com>
2022-12-15 14:48:14 -0600

    Made site static oncemore

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -12,6 +12,5 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  output: "server",
-  adapter: vercel()
+  output: "static",
 });
