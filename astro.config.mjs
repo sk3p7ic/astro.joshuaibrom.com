44b080fb66602794693cea0f2b5b9245a44a4e14
Joshua Ibrom <joshuaibrom@gmail.com>
2022-12-15 13:43:18 -0600

    Added vercel support

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+  output: "server",
+  adapter: vercel()
 });
