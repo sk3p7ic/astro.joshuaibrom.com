08888b15ab554f2aa393ea48f66812fcecb25d5a
Joshua Ibrom <joshuaibrom@gmail.com>
2024-01-22 11:55:15 -0600

    Changed "Socials" to use Socials section at bottom of home page.

--- src/components/Navbar/MobileNavigation.svelte
+++ src/components/Navbar/MobileNavigation.svelte
@@ -20,7 +20,7 @@
 {#if showMenu}
   <nav class="fixed left-0 right-0 h-full flex flex-col gap-2 nav-item transition-all" id="mobile-nav-links">
     {#each links as link}
-      <a href={link.href} class="nav-item" class:current-page="{currentPage === link.href}">{link.title}</a>
+      <a href={link.href} class="nav-item" class:current-page="{currentPage === link.href}" on:click={toggleMenu}>{link.title}</a>
     {/each}
   </nav>
 {/if}
