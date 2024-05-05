8ff8fbe5bdd4f10c263b5643295459827c5105f1
Joshua Ibrom <joshuaibrom@gmail.com>
2024-05-05 07:04:39 -0500

    Init repo for v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,8 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import svelte from "@astrojs/svelte";
-
-// https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
-  output: "static",
+  integrations: [tailwind()]
 });
