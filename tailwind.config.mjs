b0af3959b26cd45aae8fa9bd0a338628c605c9d7
Joshua Ibrom <joshuaibrom@gmail.com>
2024-05-05 07:52:38 -0500

    feat: setup project for development with carbonfox theming

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,40 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "cf-nrm-black": "#393b44",
+        "cf-nrm-red": "#c94f6d",
+        "cf-nrm-green": "#81b29a",
+        "cf-nrm-yellow": "#dbc074",
+        "cf-nrm-blue": "#719cd6",
+        "cf-nrm-magenta": "#9d79d6",
+        "cf-nrm-cyan": "#63cdcf",
+        "cf-nrm-white": "#dfdfe0",
+        'cf-dim-black': "#222222",
+        'cf-dim-red': "#ca4780",
+        'cf-dim-green': "#1fa25a",
+        'cf-dim-yellow': "#07a19e",
+        'cf-dim-blue': "#6690d9",
+        'cf-dim-magenta': "#a27fd9",
+        'cf-dim-cyan': "#2b96d9",
+        'cf-dim-white': "#bebebe",
+        'cf-brt-black': "#484848",
+        'cf-brt-red': "#f16da6",
+        'cf-brt-green': "#46c880",
+        'cf-brt-yellow': "#2dc7c4",
+        'cf-brt-blue': "#8cb6ff",
+        'cf-brt-magenta': "#c8a5ff",
+        'cf-brt-cyan': "#52bdff",
+        'cf-brt-white': "#e4e4e5",
+      },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
